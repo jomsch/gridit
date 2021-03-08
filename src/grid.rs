0da9961c2a6c7a6222b927c1d4547f56d20245ca
Simon Schniedenharn <simsch@gmail.com>
2021-03-08 19:28:46 +0100

    Add: row_iter_mut nonfunctional

--- src/grid.rs
+++ src/grid.rs
@@ -4,26 +4,23 @@ use std::mem;
 pub struct Grid<T> {
     cells: Vec<T>,
     width: usize,
-    height: usize
+    height: usize,
 }
 
-
 impl<T: Clone> Grid<T> {
-
-    pub fn new(width: usize, height: usize, default_value: T) -> Self
-    {
+    pub fn new(width: usize, height: usize, default_value: T) -> Self {
         Self {
             width,
             height,
-            cells: vec![default_value; width*height]
+            cells: vec![default_value; width * height],
         }
     }
 
     #[inline]
     fn translate(&self, x: usize, y: usize) -> usize {
-        y * self.width + x 
+        y * self.width + x
     }
-    
+
     #[inline]
     fn is_bounds(&self, x: usize, y: usize) -> bool {
         x < self.width && y < self.height
@@ -76,20 +73,24 @@ impl<T: Clone> Grid<T> {
     }
 
     pub fn row<'a>(&'a self, y: usize) -> RowIter<'a, T> {
-        let start_idx = y*self.height;
+        let start_idx = y * self.height;
         let end_idx = start_idx + self.width;
-        
+
         RowIter {
             idx: 0,
             row: &self.cells[start_idx..end_idx],
-
         }
     }
 
-    // pub fn row_mut(&self, y: usize) -> RowIter<'a, T> {
-    //     unimplemented!()
-    // }
+     pub fn row_mut<'a>(&'a mut self, y: usize) -> RowIterMut<'a, T> {
+        let start_idx = y * self.height;
+        let end_idx = start_idx + self.width;
 
+        RowIterMut {
+            idx: 0,
+            row: &mut self.cells[start_idx..end_idx],
+        }
+     }
 
     pub fn column(&self, x: usize) -> ColumnIter {
         unimplemented!()
@@ -107,11 +108,11 @@ impl<T: Clone> Grid<T> {
         unimplemented!()
     }
 
-    pub fn pattern<P: Pattern>(&self, x: usize, y: usize, p: P) -> PatternIter { 
+    pub fn pattern<P: Pattern>(&self, x: usize, y: usize, p: P) -> PatternIter {
         unimplemented!()
     }
 
-    pub fn pattern_mut<P: Pattern>(&self, x: usize, y: usize, p: P) -> PatternIter { 
+    pub fn pattern_mut<P: Pattern>(&self, x: usize, y: usize, p: P) -> PatternIter {
         unimplemented!()
     }
 
@@ -137,11 +138,25 @@ impl<'a, T> Iterator for RowIter<'a, T> {
     type Item = &'a T;
     fn next(&mut self) -> Option<Self::Item> {
         self.idx += 1;
-        self.row.get(self.idx-1) 
+        self.row.get(self.idx - 1)
     }
 }
 
+pub struct RowIterMut<'a, T> {
+    idx: usize,
+    row: &'a mut [T],
+}
 
+//impl<'a, T> Iterator for RowIterMut<'a, T> {
+//    type Item = &'a mut T;
+//    fn next(&mut self) -> Option<Self::Item> {
+//      if self.idx >= self.row.len() {
+//        return None;
+//      } 
+//      self.idx += 1;
+//      Some(&mut self.row[self.idx -1])
+//    }
+//}
 
 pub struct ColumnIter;
 pub struct NeighborIter;
@@ -150,40 +165,62 @@ pub struct PatternIter;
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn create_grid() {
         let grid = Grid::new(3, 5, 0u8);
-        assert_eq!(grid, Grid { width: 3, height: 5, cells: vec![0u8; 3*5] });
+        assert_eq!(
+            grid,
+            Grid {
+                width: 3,
+                height: 5,
+                cells: vec![0u8; 3 * 5]
+            }
+        );
     }
 
     #[test]
     fn get_cell_in_grid() {
-        let grid = Grid {width:3, height:3, cells: vec![1,1,1,1,2,1,1,1,1]};
+        let grid = Grid {
+            width: 3,
+            height: 3,
+            cells: vec![1, 1, 1, 1, 2, 1, 1, 1, 1],
+        };
         let cell = grid.get(1, 1);
         assert_eq!(cell, Some(&2));
     }
 
-
     #[test]
     fn get_mut_cell_in_grid() {
-        let mut grid = Grid {width:3, height:3, cells: vec![1,1,1,1,2,1,1,1,1]};
+        let mut grid = Grid {
+            width: 3,
+            height: 3,
+            cells: vec![1, 1, 1, 1, 2, 1, 1, 1, 1],
+        };
         let mut_cell = grid.get_mut(1, 1);
         assert_eq!(mut_cell, Some(&mut 2));
     }
 
     #[test]
     fn get_unchecked_cell_in_grid() {
-        let grid = Grid {width:3, height:3, cells: vec![1,1,1,1,2,1,1,1,1]};
+        let grid = Grid {
+            width: 3,
+            height: 3,
+            cells: vec![1, 1, 1, 1, 2, 1, 1, 1, 1],
+        };
         let cell = grid.get_unchecked(1, 1);
         assert_eq!(cell, &2);
     }
-    
+
     #[test]
     #[should_panic]
     fn get_unchecked_panic_cell_in_grid() {
-        let grid = Grid {width:3, height:3, cells: vec![1,1,1,1,2,1,1,1,1]};
-        let cell = grid.get_unchecked(3,2);
+        let grid = Grid {
+            width: 3,
+            height: 3,
+            cells: vec![1, 1, 1, 1, 2, 1, 1, 1, 1],
+        };
+        let cell = grid.get_unchecked(3, 2);
     }
 
     #[test]
@@ -208,18 +245,22 @@ mod tests {
         let mut grid = Grid::new(3, 3, 1u8);
         grid.set_unchecked(2, 3, 2u8);
     }
-    
+
     #[test]
     fn replace_cell_in_grid() {
         let mut grid = Grid::new(2, 2, 1u8);
         let value = grid.replace(1, 1, 2u8);
         assert_eq!(value, Some(1));
-        assert_eq!(grid.cells, vec![1,1,1,2]);
+        assert_eq!(grid.cells, vec![1, 1, 1, 2]);
     }
 
     #[test]
     fn row_iter() {
-        let grid = Grid { width:3, height:3, cells: vec![0, 0, 0, 1, 1, 1, 2, 2, 2] };
+        let grid = Grid {
+            width: 3,
+            height: 3,
+            cells: vec![0, 0, 0, 1, 1, 1, 2, 2, 2],
+        };
         let mut row_iter = grid.row(0);
         assert_eq!(row_iter.next(), Some(&0));
         assert_eq!(row_iter.next(), Some(&0));
@@ -233,6 +274,32 @@ mod tests {
         assert_eq!(row_iter.next(), None);
 
         let mut row_iter = grid.row(2);
+        assert_eq!(row_iter.next(), Some(&2));
+        assert_eq!(row_iter.next(), Some(&2));
+        assert_eq!(row_iter.next(), Some(&2));
+        assert_eq!(row_iter.next(), None);
+    }
+
+    #[test]
+    fn row_iter() {
+        let grid = Grid {
+            width: 3,
+            height: 3,
+            cells: vec![0, 0, 0, 1, 1, 1, 2, 2, 2],
+        };
+        let mut row_iter = grid.row_mut(0);
+        assert_eq!(row_iter.next(), Some(&0));
+        assert_eq!(row_iter.next(), Some(&0));
+        assert_eq!(row_iter.next(), Some(&0));
+        assert_eq!(row_iter.next(), None);
+
+        let mut row_iter = grid.row_mut(1);
+        assert_eq!(row_iter.next(), Some(&1));
+        assert_eq!(row_iter.next(), Some(&1));
+        assert_eq!(row_iter.next(), Some(&1));
+        assert_eq!(row_iter.next(), None);
+
+        let mut row_iter = grid.row_mut(2);
         assert_eq!(row_iter.next(), Some(&2));
         assert_eq!(row_iter.next(), Some(&2));
         assert_eq!(row_iter.next(), Some(&2));
