7996d16c38105c8f5d0eac4eaf7a36d6fada96fc
jomsch <jomsch@jomsch.de>
2021-03-30 23:15:44 +0200

    add PositionsEnumerator for GridIter

--- src/grid/position.rs
+++ src/grid/position.rs
@@ -1,21 +1,23 @@
-use crate::grid::Position;
+use super::grid::Position;
 
-pub(crate) struct Positions<I> {
-    iter: I,
-    next_pos: Box<Fn(&I, Option<Position>) -> Position>,
-    prev_position: Option<Position>,
+pub struct Positions<I> {
+    pub(crate) inner: I,
+    pub(crate) next_pos: Box<dyn Fn(&I, Option<Position>) -> Position>,
+    pub(crate) prev_position: Option<Position>,
 }
 
-trait PositionsEnumerator<I: Iterator> {
-    pub fn positions(self) -> Positions<I> {
-        Position { iter: self }
-    }
+pub(crate) trait PositionsEnumerator 
+where
+    Self: Sized,
+{
+    fn positions(self) -> Positions<Self>;
 }
 
 impl<I: Iterator> Iterator for Positions<I> {
     type Item = (Position, I::Item);
     fn next(&mut self) -> Option<Self::Item> {
-        let next_pos = self.next_pos(&self.iter, self.prev_position);
-        Some((next_pos, self.iter.next()?))
-    }
+        let next_pos = (self.next_pos)(&self.inner, self.prev_position);
+        self.prev_position = Some(next_pos);
+        Some((next_pos, self.inner.next()?))
+   }
 }
