f0cfb87f55325a34deef1ccad38637310884d800
jomsch <jomsch@jomsch.de>
2021-04-27 09:45:04 +0200

    add hdpi scale factor check, add resize function to Board

--- examples/board/board.rs
+++ examples/board/board.rs
@@ -1,7 +1,8 @@
 use ggez::{graphics, Context, ContextBuilder, GameResult};
 use ggez::graphics::{
-    size, BlendMode, Color, DrawMode, DrawParam, Drawable, FillOptions, Mesh, Rect,
+    size, BlendMode, Color, DrawMode, DrawParam, Drawable, FillOptions, StrokeOptions, Mesh, Rect,
 };
+use ggez::mint::Point2;
 
 use gridit::Grid;
 use gridit::PositionsEnumerator;
@@ -21,20 +22,32 @@ pub struct Field {
 
 pub struct Board {
     pub grid :Grid<Field>,
-    pub xy: (f32, f32),
-    pub size: f32,
+    rect: Rect,
 }
 
 impl Board {
     pub fn new(grid: Grid<Field>, xy: (f32, f32), size: f32) -> Self {
-        Self { grid, xy, size }
+        let rect = Rect::new(xy.0, xy.1, size, size);
+        Self { grid, rect }
+    }
+
+    pub fn contains_point(&self, point: Point2<f32>) -> bool {
+        self.rect.contains(point)
+    }
+
+    pub fn get_rect(&self) -> Rect {
+        self.rect
+    }
+
+    pub fn set_rect(&mut self, rect: Rect) {
+        self.rect = rect;
     }
 }
 
 impl Drawable for Board {
-    fn draw(&self, ctx: &mut Context, param: DrawParam) -> GameResult<()> {
-        let (bx, by) = self.xy;
-        let rect_size = self.size / 8.0;
+    fn draw(&self, ctx: &mut Context, _ : DrawParam) -> GameResult<()> {
+        let (bx, by) = (self.rect.x, self.rect.y);
+        let rect_size = self.rect.w / 8.0;
 
         for ((x, y), field) in self.grid.iter().positions() {
             let (x, y) = (x as f32, y as f32);
@@ -47,15 +60,21 @@ impl Drawable for Board {
                 rect,
                 field.bg_color,
             )?;
-            graphics::draw(ctx, &mrect, DrawParam::default());
+            graphics::draw(ctx, &mrect, DrawParam::default())?;
         }
+        let mrect = Mesh::new_rectangle(
+            ctx,
+            DrawMode::Stroke(StrokeOptions::default()),
+            self.rect,
+            Color::BLACK,
+        )?;
+        graphics::draw(ctx, &mrect, DrawParam::default())?;
 
         Ok(())
     }
 
     fn dimensions(&self, ctx: &mut Context) -> Option<Rect> {
-        let (width, height) = size(&ctx);
-        Some(Rect::new(0.0, 0.0, width, height))
+        Some(self.rect)
     }
 
     fn set_blend_mode(&mut self, mode: Option<BlendMode>) {}
--- examples/board/main.rs
+++ examples/board/main.rs
@@ -1,6 +1,10 @@
-use ggez::event::{self, EventHandler};
+use ggez::event::{self, EventHandler, MouseButton};
+use ggez::input::mouse;
+use ggez::input::keyboard::{KeyCode, KeyMods};
+use ggez::mint::Point2;
 use ggez::graphics::DrawParam;
 use ggez::{graphics, Context, ContextBuilder, GameResult};
+use ggez::graphics::Rect;
 use gridit::Grid;
 use gridit::PositionsEnumerator;
 
@@ -9,20 +13,22 @@ use crate::board::*;
 
 
 fn main() {
-    let (mut ctx, mut event_loop) = ContextBuilder::new("pawn_example", "")
+    let (mut ctx, event_loop) = ContextBuilder::new("pawn_example", "")
         .build()
         .expect("Could not create context");
 
-    let mut game = BoardGame::new(&mut ctx);
+    let game = BoardGame::new(&mut ctx);
     event::run(ctx, event_loop, game);
 }
 
 struct BoardGame {
     board: Board,
+    has_resized: bool,
+    hdpi_factor: f32,
 }
 
 impl BoardGame {
-    fn new(_ctx: &mut Context) -> Self {
+    fn new(ctx: &mut Context) -> Self {
         let mut grid = Grid::new(
             8,
             8,
@@ -38,19 +44,45 @@ impl BoardGame {
             field.default_color = WHITE;
             field.bg_color = WHITE;
         }
+        let hdpi_factor = graphics::window(&ctx).scale_factor() as f32;
 
-        Self { board: Board::new(grid, (50.0, 50.0), 400.0) }
+        Self { 
+            board: Board::new(grid, (50.0, 50.0), 400.0),
+            has_resized: true,
+            hdpi_factor,
+        }
     }
 }
 
 impl EventHandler for BoardGame {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        let mpos = mouse::position(ctx);
+        let lbtn_pressed = mouse::button_pressed(ctx,  MouseButton::Left);
+
+        if self.board.contains_point(mpos) && lbtn_pressed {
+            println!("{}, {}, MouseButton Pressed: {}", mpos.x, mpos.y, lbtn_pressed);
+            let mut brect = self.board.get_rect();
+            brect.w += 5.0;
+            brect.h += 5.0;
+            self.board.set_rect(brect);
+        }
+
+    
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx, graphics::Color::new(0.25, 0.25, 0.25, 1.0));
-        graphics::draw(ctx, &self.board, DrawParam::default());
+        let hdpi_factor = graphics::window(&ctx).scale_factor() as f32;
+        if self.has_resized || self.hdpi_factor != hdpi_factor {
+            let hdpi_factor = graphics::window(&ctx).scale_factor() as f32;
+            let (x, y) = graphics::size(&ctx);
+            let draw_rect = Rect::new(0.0, 0.0, x*hdpi_factor, y*hdpi_factor);
+            graphics::set_screen_coordinates(ctx, draw_rect)?;
+            self.has_resized = false;
+        }
+
+        graphics::draw(ctx, &self.board, DrawParam::default())?;
         graphics::present(ctx)
     }
 }
